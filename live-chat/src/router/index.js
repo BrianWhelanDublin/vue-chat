47a5849abc826c5120babdc89e71d3bb8d7fcbcd
Brian Whelan <brian@friday.ie>
2021-07-19 08:44:11 +0100

    finish project

--- live-chat/src/router/index.js
+++ live-chat/src/router/index.js
@@ -3,7 +3,7 @@ import Welcome from "../views/Welcome.vue"
 import Chatroom from '../views/Chatroom.vue';
 import { projectAuth } from '../firebase/config';
 
-// routh gaurd 
+// routh gaurds
 const requireAuth = (to, from, next) => {
     let user = projectAuth.currentUser;
     if (!user) {
@@ -13,10 +13,20 @@ const requireAuth = (to, from, next) => {
     }
 }
 
+const requireNoAuth = (to, from, next) => {
+    let user = projectAuth.currentUser;
+    if (user) {
+        next({ name: "Chatroom" })
+    } else {
+        next()
+    }
+}
+
 const routes = [{
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: requireNoAuth
 }, {
     path: '/chatroom',
     name: 'Chatroom',
